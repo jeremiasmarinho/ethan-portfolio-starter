327a675c31f330849f11346248cd95f7b8378bc4
jeremiasmarinho <jeremias10@msn.com>
2023-11-27 03:13:43 -0300

    Montando o services

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,41 +1,49 @@
 // icons
 import {
   RxCrop,
-  RxPencil2,
   RxDesktop,
+  RxPencil2,
   RxReader,
   RxRocket,
+  RxArrowTopRight,
 } from "react-icons/rx";
-
 
 // data
 const serviceData = [
   {
     icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Branding",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Development",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Copywriting",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "SEO",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
 ];
+
+import { Swiper, SwiperSlider } from "swiper/react";
+
+import { FreeMode, Pagination } from "swiper";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 
 const ServiceSlider = () => {
   return <div>Service Slider</div>;
