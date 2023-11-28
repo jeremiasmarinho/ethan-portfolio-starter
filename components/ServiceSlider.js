cd6783486645652e88bd6ae9c6d6f8881408efde
jeremiasmarinho <jeremias10@msn.com>
2023-11-28 04:24:50 -0300

    Quebrando cabeça

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -68,15 +68,16 @@ const ServiceSlider = () => {
       {serviceData.map((item, index) => {
         return (
           <SwiperSlide key={index}>
-            <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer">
-              <div>{item.icon}</div>
-            </div>
-            <div>
-              <div>{item.title}</div>
-              <p>{item.description}</p>
-            </div>
-            <div className="text-3xl">
-              <RxArrowTopRight />
+            <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+              <div className="text-4xl text-accent mb-4">{item.icon}</div>
+
+              <div>
+                <div>{item.title}</div>
+                <p>{item.description}</p>
+              </div>
+              <div className="text-3xl">
+                <RxArrowTopRight />
+              </div>
             </div>
           </SwiperSlide>
         );
