cc7abae71152fac7ccb3216154cda7b2845497fa
jeremiasmarinho <jeremias10@msn.com>
2023-12-01 01:16:49 -0300

    services concluido

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,3 +1,9 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
 // icons
 import {
   RxCrop,
@@ -9,7 +15,7 @@ import {
 } from "react-icons/rx";
 
 // data
-const serviceData = [
+export const serviceData = [
   {
     icon: <RxCrop />,
     title: "Branding",
@@ -37,13 +43,7 @@ const serviceData = [
   },
 ];
 
-import { Swiper, SwiperSlide, SwiperSlider } from "swiper/react";
-
 import { FreeMode, Pagination } from "swiper";
-
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
 
 const ServiceSlider = () => {
   return (
@@ -71,12 +71,14 @@ const ServiceSlider = () => {
             <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
               <div className="text-4xl text-accent mb-4">{item.icon}</div>
 
-              <div>
-                <div>{item.title}</div>
-                <p>{item.description}</p>
+              <div className="mb-8">
+                <div className="mb-2 text-lg">{item.title}</div>
+                <p className="max-w-[350px] leading-normal">
+                  {item.description}
+                </p>
               </div>
               <div className="text-3xl">
-                <RxArrowTopRight />
+                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
               </div>
             </div>
           </SwiperSlide>
--- pages/services/index.js
+++ pages/services/index.js
@@ -12,15 +12,35 @@ const Services = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-x-8">
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8">
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
               My services <span className="text-accent">.</span>
-            </h2>
-            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
               LoremSunt est cillum dolore veniam minim cupidatat eiusmod.
               LoremSunt est cillum dolore veniam minim cupidatat eiusmod.
-            </p>
+            </motion.p>
           </div>
-          <ServiceSlider />
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            <ServiceSlider />
+          </motion.div>
         </div>
       </div>
       <Buld />
