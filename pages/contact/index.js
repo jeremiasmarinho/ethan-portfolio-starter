271332fdbca0a3e63fd0488ea7904b5647b80c5d
jeremiasmarinho <jeremias10@msn.com>
2023-12-02 05:20:14 -0300

    finalizando

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -25,7 +25,7 @@ const Contact = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="flex-1 flex flex-col gap-6 w-full mx-auto"
+            className="flex-1 flex text-xs xl:text-lg flex-col gap-6 w-full mx-auto"
           >
             <div className="flex gap-x-6 w-full">
               <input type="text" placeholder="Nome" className="input" />
